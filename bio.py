bc3d860fa81de8d5aa8d9c92878d11f8750131a5
Ian <ianjenatz@ufm.edu>
2019-10-01 12:55:35 -0600

    Modified py file to send more variables

--- bio.py
+++ bio.py
@@ -3,23 +3,7 @@ import yaml
 
 app = Flask(__name__)
 
-# my_list = []
-# with open("links.yaml", 'r') as f:
-#     file = yaml.safe_load(f)
-    # line = f.readline()
-    # while line:
-    #     my_list.append(line)
-    #     line = f.readline()
-
-# txt = file["links"]["facebook"]["link"]
-
 my_dict = yaml.safe_load(open("links.yaml"))
-# link_dict = {}
-# for item in "links":
-#   link_dict.append(item)
-
-facebook = my_dict["links"][0]["facebook"]["link"]
-print(facebook)
 
 @app.route("/") # info page
 def info():
@@ -28,18 +12,35 @@ def info():
     nombre = my_dict["nombre"],
     short_bio = my_dict["short_bio"],
     imagen = my_dict["imagen"],
+
     fb_description = my_dict["links"][0]["facebook"]["description"],
     fb_link = my_dict["links"][0]["facebook"]["link"],
+    fb_enable = my_dict["links"][0]["facebook"]["enable"],
+    
     ig_description = my_dict["links"][1]["instagram"]["description"],
     ig_link = my_dict["links"][1]["instagram"]["link"],
+    ig_enable = my_dict["links"][1]["instagram"]["enable"],
+    
+    bg_description = my_dict["links"][2]["blog"]["description"],
+    bg_link = my_dict["links"][2]["blog"]["link"],
+    bg_enable = my_dict["links"][2]["blog"]["enable"],
+    
+    gh_description = my_dict["links"][3]["github"]["description"],
+    gh_link = my_dict["links"][3]["github"]["link"],
+    gh_enable = my_dict["links"][3]["github"]["enable"],
+
+    yt_description = my_dict["links"][4]["youtube"]["description"],
+    yt_link = my_dict["links"][4]["youtube"]["link"],
+    yt_enable = my_dict["links"][4]["youtube"]["enable"],
+
+    al_description = my_dict["links"][5]["algo"]["description"],
+    al_link = my_dict["links"][5]["algo"]["link"],
+    al_enable = my_dict["links"][5]["algo"]["enable"],
+
+    # yt_description = my_dict["links"][4]["youtube"]["description"],
+    # yt_link = my_dict["links"][4]["youtube"]["link"],
+    # yt_enable = my_dict["links"][4]["youtube"]["enable"],
     )
     
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-# Ideas:
-# Maybe use {{% if enable == True %}} before the whole div so that block only appears when 
-# enable True. Or, create a variable like the teacher did with Developer. (Not sure how to 
-# pass this for each website button though)
